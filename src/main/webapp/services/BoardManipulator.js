5bdd772a48a04f9b2172820aca28f720d1838ee8
xiezhiyan <1715380210@qq.com>
2017-08-14 18:09:58 +0800

    前端

---
+++ src/main/webapp/services/BoardManipulator.js
@@ -0,0 +1,58 @@
+/*jshint undef: false, unused: false, indent: 2*/
+/*global angular: false */
+
+
+'use strict';
+
+angular.module('demoApp').factory('BoardManipulator', ['$resource',function ($resource) {
+
+    var base_url = "http://localhost:9222/backlog";
+
+  return {
+
+    addColumn: function (board, columnName) {
+      board.columns.push(new Column(columnName));
+    },
+
+    addCardToColumn: function (board, column, cardTitle, details) {
+      angular.forEach(board.columns, function (col) {
+        if (col.name === column.name) {
+            var card = new Card(cardTitle, column.name, details);
+            col.cards.push(card);
+            var Backlog = $resource(base_url, {});
+            //Backlog.save({}, card);
+        }
+      });
+    },
+    removeCardFromColumn: function (board, column, card) {
+      angular.forEach(board.columns, function (col) {
+        if (col.name === column.name) {
+          col.cards.splice(col.cards.indexOf(card), 1);
+        }
+      });
+    },
+    addBacklog: function (board, backlogName) {
+      board.backlogs.push(new Backlog(backlogName));
+    },
+
+    addPhaseToBacklog: function (board, backlogName, phase) {
+      angular.forEach(board.backlogs, function (backlog) {
+        if (backlog.name === backlogName) {
+          backlog.phases.push(new Phase(phase.name));
+        }
+      });
+    },
+
+    addCardToBacklog: function (board, backlogName, phaseName, task) {
+      angular.forEach(board.backlogs, function (backlog) {
+        if (backlog.name === backlogName) {
+          angular.forEach(backlog.phases, function (phase) {
+            if (phase.name === phaseName) {
+              phase.cards.push(new Card(task.title, task.status, task.details));
+            }
+          });
+        }
+      });
+    }
+  };
+}]);
